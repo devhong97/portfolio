21f5026b6a5675859e108246542f6fecf1889963
devhong97 <128722043+devhong97@users.noreply.github.com>
2023-03-28 16:43:25 +0900

    Update main.js
    메인페이지 하단 위로가기버튼 스크롤 관련 이벤트 추가

---
+++ resource/js/main.js
@@ -0,0 +1,22 @@
+/* 위로가기 버튼 */
+$(document).ready(function(){
+  $("#toTop").hide();
+
+  // 스크롤 시(50px 초과) toTop 나타남  
+  $(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+	// toTop a태그 클릭 시 최상단으로(속도 100)
+    $('#toTop a').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+  });
+});
